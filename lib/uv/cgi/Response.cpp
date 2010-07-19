40dd1a67f11ebcb1095475b36494ad74efb0d02f
sumpygump@gmail.com <sumpygump@gmail.com@2eb6a3c6-e49c-db6b-d730-ce7ab59522c8>
2010-07-19 12:27:16 +0000

    Added CGI Response object

---
+++ lib/uv/cgi/Response.cpp
@@ -0,0 +1,25 @@
+/*~
+ * Ultraviolet MVC
+ * Copyright (C) 2010 Lost Mind Software
+ *
+ * Licensed under the MIT license: http://www.opensource.org/licenses/mit-license.php
+ *
+ * @version $Id$
+ */
+
+/** @file Response.cpp
+ * Ultraviolet CGI Response class definition.
+ *
+ * Handles HTTP responses.
+ */
+
+#include <iostream>
+
+#include "Response.h"
+
+/**
+ *
+ */
+uv::Response::Response()
+{
+}
